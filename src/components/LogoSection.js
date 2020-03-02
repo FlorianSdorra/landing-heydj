242e26160ab6cda7533166b06bdb4fcbc97ffa12
FlorianSdorra <fsd114@posteo.de>
2020-03-02 20:21:33 +0100

    styled more darkmode

--- src/components/LogoSection.js
+++ src/components/LogoSection.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
-const LogoSection = () =>{
-    return(
-        <div className="logo-section-wrapper">
-            <div className="logo-circle"></div>
-            <h3>lorem poporem ipsum malakia </h3>
-            <form className="search-form">
-                <label>
-                    <div className="search-form-input-border">
-                        <input className="search-form-input" type="text" placeholder="SEARCH"></input>
+class LogoSection extends React.Component{
+
+    render(){
+        // const search = <i class="fas fa-search"></i>;
+
+        return(
+            <div className="logo-section-wrapper">
+                <div className="logo-circle"></div>
+                <h3>lorem poporem ipsum malakia </h3>
+                <div className="search-form">
+                    <div className="icon-wrap">
+                        
                     </div>
-                </label>
-            </form>
-        </div>
-    )
+                    
+                    <form>
+                        <label>
+                            <div className="search-form-input-border">
+                                <i class="fas fa-search search-icon"></i>
+                                <input className="search-form-input" type="text" ></input>
+                            </div>
+                        </label>
+                    </form>
+                </div>
+                    
+                
+            </div>
+        )
+    }
+    
 }
 
 export default LogoSection;
