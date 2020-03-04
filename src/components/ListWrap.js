a7bb13b6df90ce6545ac88f1f9afe579b444f411
FlorianSdorra <fsd114@posteo.de>
2020-03-04 15:25:53 +0100

    lists elements roughly exist now / figuring out media queries now

--- src/components/ListWrap.js
+++ src/components/ListWrap.js
@@ -4,8 +4,43 @@ class ListWrap extends React.Component{
 
     render(){
         return(
-            <div className="list-wrap">
-                <p>hello</p>
+            <div className="result-list-wrap">
+                <div className="result-list-item">
+                    <div className="result-image-container">
+                        <div className="result-image"></div>
+                    </div>
+                    <div className="result-info-container">
+                        <h3>This is a result</h3>
+                        <h4>Some info</h4>
+                    </div>
+                </div>
+                <div className="result-list-item">
+                    <div className="result-image-container">
+                        <div className="result-image"></div>
+                    </div>
+                    <div className="result-info-container">
+                        <h3>This is a result</h3>
+                        <h4>Some info</h4>
+                    </div>
+                </div>
+                <div className="result-list-item">
+                    <div className="result-image-container">
+                        <div className="result-image"></div>
+                    </div>
+                    <div className="result-info-container">
+                        <h3>This is a result</h3>
+                        <h4>Some info</h4>
+                    </div>
+                </div>
+                <div className="result-list-item">
+                    <div className="result-image-container">
+                        <div className="result-image"></div>
+                    </div>
+                    <div className="result-info-container">
+                        <h3>This is a result</h3>
+                        <h4>Some info</h4>
+                    </div>
+                </div>
             </div>
         )
     }
